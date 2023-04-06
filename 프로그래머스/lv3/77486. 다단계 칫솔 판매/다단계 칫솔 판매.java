9913c059dc96d59fe482b228ccd62d08811f2e28
Sparkle <109789965+RadSparkle@users.noreply.github.com>
2023-04-07 00:30:30 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 43.21 ms, Memory: 131 MB -BaekjoonHub

--- 프로그래머스/lv3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
+++ 프로그래머스/lv3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -2,49 +2,28 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
-        int n = enroll.length;
-        Map<String, Integer> indexMap = new HashMap<>();
-        Map<String, Node> nodeMap = new HashMap<>();
-
-        for (int i = 0; i < n; i++) {
-            indexMap.put(enroll[i], i);
-            nodeMap.put(enroll[i], new Node(enroll[i]));
+        Map<String, String> parent = new HashMap<>();
+        Map<String, Integer> profit = new HashMap<>();
+        for (int i = 0; i < enroll.length; i++) {
+            parent.put(enroll[i], referral[i]);
+            profit.put(enroll[i], 0);
         }
-
-        for (int i = 0; i < n; i++) {
-            String parent = referral[i];
-            if (!parent.equals("-")) {
-                nodeMap.get(enroll[i]).parent = nodeMap.get(parent);
+        for (int i = 0; i < seller.length; i++) {
+            String cur = seller[i];
+            int curAmount = amount[i] * 100;
+            while (!cur.equals("-")) {
+                int parentAmount = curAmount / 10;
+                int curProfit = curAmount - parentAmount;
+                profit.put(cur, profit.get(cur) + curProfit);
+                cur = parent.get(cur);
+                curAmount = parentAmount;
+                if (parentAmount == 0) break;
             }
         }
-
-        int m = seller.length;
-        for (int i = 0; i < m; i++) {
-            Node sellerNode = nodeMap.get(seller[i]);
-            int sellAmount = amount[i] * 100;
-            while (sellerNode != null && sellAmount > 0) {
-                int commission = sellAmount / 10;
-                sellerNode.profit += sellAmount - commission;
-                sellAmount = commission;
-                sellerNode = sellerNode.parent;
-            }
+        int[] answer = new int[enroll.length];
+        for (int i = 0; i < enroll.length; i++) {
+            answer[i] = profit.get(enroll[i]);
         }
-
-        int[] answer = new int[n];
-        for (int i = 0; i < n; i++) {
-            answer[i] = nodeMap.get(enroll[i]).profit;
-        }
-
         return answer;
-    }
-
-    static class Node {
-        String name;
-        int profit;
-        Node parent;
-
-        public Node(String name) {
-            this.name = name;
-        }
     }
 }
