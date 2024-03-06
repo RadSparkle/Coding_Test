0964b91199e6e267cd4348c75d6be972243484a1
Sparkle <109789965+RadSparkle@users.noreply.github.com>
2024-03-06 23:56:42 +0900

    [Bronze II] Title: 트럭 주차, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2979. 트럭 주차/트럭 주차.py
+++ 백준/Bronze/2979. 트럭 주차/트럭 주차.py
@@ -1,18 +1,27 @@
-a, b, c = map(int,input().split())
+a, b, c = map(int, input().split())
 
-time = [[] for _ in range(100)]
-answer = 0
-for _ in range(3):
-    x,y = map(int,input().split())
-    for i in range(x,y):
-        time[i].append(1)
-for i in time:
-    if sum(i) == 1:
-        answer += sum(i)*a
-    elif sum(i) ==2:
-        answer += sum(i)*b
-    elif sum(i) ==3:
-        answer += sum(i)*c
-    else :
-        answer +=0
-print(answer)
+t1, t2 = map(int, input().split())
+t3, t4 = map(int, input().split())
+t5, t6 = map(int, input().split())
+
+time_line = [[] for _ in range(max(t1, t2, t3, t4, t5, t6) + 1)]
+
+for i in range(t1+1, t2 + 1):
+    time_line[i].append(1)
+
+for i in range(t3+1, t4 + 1):
+    time_line[i].append(1)
+
+for i in range(t5+1, t6 + 1):
+    time_line[i].append(1)
+
+result = 0
+
+for i in time_line:
+    if len(i) == 1:
+        result += len(i) * a
+    if len(i) == 2:
+        result += len(i) * b
+    if len(i) == 3:
+        result += len(i) * c
+print(result)
