7f485be62660c24e1697624fb442b6ab529db069
Sparkle <109789965+RadSparkle@users.noreply.github.com>
2024-06-21 16:09:16 +0900

    [Silver II] Title: DFS와 BFS, Time: 368 ms, Memory: 38396 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,33 +1,33 @@
-from collections import deque
-
 N, M, V = map(int, input().split())
 
-graph = [[False] * (N+1) for _ in range(N+1)]
+graph = [[0] * (N+1) for _ in range(N+1)]
 
 for _ in range(M):
     a, b = map(int, input().split())
-    graph[a][b] = True
-    graph[b][a] = True
-visited1 = [False] * (N + 1)
-visited2 = [False] * (N + 1)
+    graph[a][b] = graph[b][a] = 1
 
-def bfs(V):
-    q = deque([V])
-    visited2[V] = True
-    while q:
-        V= q.popleft()
-        print(V, end=" ")
-        for i in range(1, N+1):
-            if not visited2[i] and graph[V][i]:
-                q.append(i)
-                visited2[i] = True
+visited1 = [0] * (N+1)
+visited2 = visited1.copy()
 
 def dfs(V):
-    visited1[V] = True
-    print(V, end = " ")
-    for i in range(1, N+1):
-        if not visited1[i] and graph[V][i]:
+    visited1[V] = 1
+
+    print(V, end = ' ')
+    for i in range(N+1):
+        if graph[V][i] == 1 and visited1[i] == 0:
             dfs(i)
+
+def bfs(V):
+    visited2[V] = 1
+    queue = [V]
+
+    while queue:
+        V = queue.pop(0)
+        print(V, end =' ')
+        for i in range(1, N+1):
+            if graph[V][i] == 1 and visited2[i] == 0:
+                queue.append(i)
+                visited2[i] = 1
 
 dfs(V)
 print()
