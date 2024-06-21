650fdd317eeb5399b0af6a9ff943baaa9bc6d678
Sparkle <109789965+RadSparkle@users.noreply.github.com>
2024-06-21 16:22:56 +0900

    [Bronze II] Title: 거스름돈, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/5585. 거스름돈/거스름돈.py
+++ 백준/Bronze/5585. 거스름돈/거스름돈.py
@@ -1,8 +1,12 @@
-coin = [500,100,50,10,5,1]
-money = 1000-int(input())
-answer = 0
+coin = [500, 100, 50, 10, 5, 1]
+
+price = 1000-int(input())
+changes_cnt = 0
+
 for i in coin:
-    a,b = divmod(money, i)
-    answer += a
-    money = b
-print(answer)
+    a, b = divmod(price, i)
+    if a > 0:
+        changes_cnt+=a
+        price = b
+
+print(changes_cnt)
