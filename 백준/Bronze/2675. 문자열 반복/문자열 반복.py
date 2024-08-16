e0ec229266576537d10975e670f6aa4843d60d24
Sparkle <109789965+RadSparkle@users.noreply.github.com>
2024-08-16 14:42:02 +0900

    [Bronze II] Title: 문자열 반복, Time: 28 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.py
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.py
@@ -1,7 +1,14 @@
-n = int(input())
+T = int(input())
+answer = []
 
-for _ in range(n):
-    cnt, word = input().split()
-    for x in word:
-        print(x*int(cnt), end='') 
-    print()
+while T > 0:
+    a, b = input().split()
+    string_list = [i for i in b]
+    tmp = []
+    for i in string_list:
+        tmp.append(i*int(a))
+    answer.append(tmp)
+    T-=1
+
+for i in answer:
+    print(''.join(i))
