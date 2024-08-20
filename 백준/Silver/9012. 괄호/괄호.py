1a8d2b7c05d86b2e8a52e9a7bb2f2345672b8e45
Sparkle <109789965+RadSparkle@users.noreply.github.com>
2024-08-21 04:08:53 +0900

    [Silver IV] Title: 괄호, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -0,0 +1,18 @@
+
+for _ in range(int(input())):
+    a = input()
+    stack = []
+    for i in a:
+        if i == '(':
+            stack.append(i)
+        elif i == ')':
+            if stack:
+                stack.pop()
+            else:
+                print('NO')
+                break
+    else:
+        if stack:
+            print('NO')
+        else:
+            print('YES')
