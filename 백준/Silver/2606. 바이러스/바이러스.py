2371ad40a963b3452015596b9e5fcd8504bdd706
Sparkle <109789965+RadSparkle@users.noreply.github.com>
2024-08-29 00:34:40 +0900

    [Silver III] Title: 바이러스, Time: 56 ms, Memory: 34068 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,23 +1,26 @@
-import sys
-n = int(sys.stdin.readline())
-m = int(sys.stdin.readline())
-graph = [[] * n for _ in range(n + 1)]
+from collections import deque
+
+def dfs(graph, start):
+    visited = set()
+
+    queue = deque([start])
+    count=-1
+    while queue:
+        node = queue.popleft()
+        if node not in visited:
+            visited.add(node)
+            count+=1
+            for neighor in range(len(graph[node])):
+                if graph[node][neighor] and neighor not in visited:
+                    queue.append(neighor)
+    return count
+n = int(input())
+m = int(input())
+graph = [[0 for i in range(n+1)] for i in range(n+1)]
 
 for _ in range(m):
     a, b = map(int, input().split())
-    graph[a].append(b)
-    graph[b].append(a)
+    graph[a][b] = 1
+    graph[b][a] = 1
 
-cnt = 0
-visited = [0] * (n + 1)
-
-
-def dfs(start):
-    global cnt
-    visited[start] = 1
-    for i in graph[start]:
-        if visited[i] == 0:
-            dfs(i)
-            cnt += 1
-dfs(1)
-print(cnt)
+print(dfs(graph, 1))
