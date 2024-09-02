4d9e683c33b4b203c5c8f73d77ab28902f625a52
Sparkle <109789965+RadSparkle@users.noreply.github.com>
2024-09-02 20:31:20 +0900

    [Silver I] Title: 미로 탐색, Time: 80 ms, Memory: 34176 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -2,26 +2,26 @@ from collections import deque
 
 def bfs(x, y):
     queue = deque()
-    queue.append((x,y))
+    queue.append((x, y))
+    count = 1
     while queue:
         x, y = queue.popleft()
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if nx < 0 or nx >= n or ny < 0 or ny >= m:
-                continue
-            if graph[nx][ny] == 0:
-                continue
-            if graph[nx][ny] == 1:
-                graph[nx][ny] = graph[x][y] + 1
-                queue.append((nx, ny))
-    return graph[n-1][m-1]
+            nx = dx[i] + x
+            ny = dy[i] + y
 
-n, m = map(int,input().split())
-graph = []
-for i in range(n):
-    graph.append(list(map(int,input())))
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+            if 0 <= nx < N and 0 <= ny < M and maze[nx][ny] == 1:
+                maze[nx][ny] = maze[x][y]+1
+                queue.append((nx, ny))
+    return maze[N-1][M-1]
+
+
+
+N, M = map(int, input().split())
+maze = [list(map(int, input())) for _ in range(N)]
+
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
 
 print(bfs(0, 0))
+
