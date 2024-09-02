3fc8b5540e7594b56829275591752adde49e1d57
Sparkle <109789965+RadSparkle@users.noreply.github.com>
2024-09-02 20:41:53 +0900

    [Silver I] Title: 미로 탐색, Time: 68 ms, Memory: 34176 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,20 +1,21 @@
 from collections import deque
 
+
 def bfs(x, y):
     queue = deque()
-    queue.append((x, y))
-    count = 1
+    queue.append((x,y))
+    
     while queue:
         x, y = queue.popleft()
+        
         for i in range(4):
             nx = dx[i] + x
             ny = dy[i] + y
-
-            if 0 <= nx < N and 0 <= ny < M and maze[nx][ny] == 1:
-                maze[nx][ny] = maze[x][y]+1
+            
+            if 0<= nx < N and 0<= ny < M and maze[nx][ny] == 1:
+                maze[nx][ny] = maze[x][y] + 1
                 queue.append((nx, ny))
     return maze[N-1][M-1]
-
 
 
 N, M = map(int, input().split())
@@ -24,4 +25,3 @@ dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
 
 print(bfs(0, 0))
-
