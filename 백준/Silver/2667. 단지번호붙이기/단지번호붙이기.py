1e6839da91d585770a1e217d181346fd8375c828
Sparkle <109789965+RadSparkle@users.noreply.github.com>
2024-09-02 21:33:48 +0900

    [Silver I] Title: 단지번호붙이기, Time: 48 ms, Memory: 34072 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,39 +1,35 @@
 from collections import deque
 
-def bfs(graph, x, y):
+def bfs(x, y):
     queue = deque()
     queue.append((x,y))
-    n = len(graph)
-    cnt=1
-    graph[x][y] = 0
+    map[x][y]=0
+    count = 1
     while queue:
-        x,y = queue.popleft()
+        x, y = queue.popleft()
+
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if nx< 0 or ny < 0 or nx>=n or ny>= n:
-                continue
-            if graph[nx][ny] == 1:
-                graph[nx][ny] = 0
-                queue.append((nx,ny))
-                cnt+=1
-    return cnt
 
-n = int(input())
-graph=[]
-group_cnt=0
-answer=[]
-for _ in range(n):
-    graph.append(list(map(int,input())))
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
+            if 0<= nx < N and 0<= ny < N and map[nx][ny] == 1:
+                count+=1
+                map[nx][ny] = 0
+                queue.append((nx, ny))
+    return count
+N = int(input())
 
-for i in range(n):
-    for j in range(n):
-        if graph[i][j] == 1:
-            answer.append(bfs(graph,i,j))
+map = [list(map( int, input())) for _ in range(N)]
+count = 0
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+result= []
 
-print(len(answer))
-answer.sort()
-for i in answer:
+for i in range(N):
+    for j in range(N):
+        if map[i][j] == 1:
+            result.append(bfs(i,j))
+print(len(result))
+result.sort()
+for i in result:
     print(i)
