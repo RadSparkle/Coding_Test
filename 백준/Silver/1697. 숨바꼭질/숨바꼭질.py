980b61bd71e515b51d2e2714044dffc6170c34f3
Sparkle <109789965+RadSparkle@users.noreply.github.com>
2024-09-27 17:10:59 +0900

    [Silver I] Title: 숨바꼭질, Time: 84 ms, Memory: 35300 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,28 +1,17 @@
 from collections import deque
 
-def bfs(n, k):
-    queue = deque()
-    queue.append(n)
-    visited = [False] * 100001  # 0부터 100000까지 총 100001개
-    visited[n] = True
-    time = 0
-
-    while queue:
-        size = len(queue)
-        for i in range(size):
-            cur = queue.popleft()
-            if cur == k:
-                return time
-            if cur - 1 >= 0 and not visited[cur - 1]:
-                queue.append(cur - 1)
-                visited[cur - 1] = True
-            if cur + 1 <= 100000 and not visited[cur + 1]:
-                queue.append(cur + 1)
-                visited[cur + 1] = True
-            if cur * 2 <= 100000 and not visited[cur * 2]:
-                queue.append(cur * 2)
-                visited[cur * 2] = True
-        time += 1
-
 n, k = map(int, input().split())
-print(bfs(n, k))
+visited = [0] * 100001
+MAX = 100001
+
+def bfs(n):
+    queue = deque([n])
+    while queue:
+        x = queue.popleft()
+        if x == k:
+            return visited[x]
+        for i in (x-1, x+1, x*2):
+            if 0 <= i < MAX and not visited[i]:
+                visited[i] = visited[x] + 1
+                queue.append(i)
+print(bfs(n))
