b18bba0dc0698b96a30767d710c978d57e7301f8
Sparkle <109789965+RadSparkle@users.noreply.github.com>
2024-10-08 02:26:40 +0800

    [Silver II] Title: 유기농 배추, Time: 216 ms, Memory: 34088 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,36 +1,37 @@
-import sys
+from collections import deque
 
-sys.setrecursionlimit(10000)
+t = int(input())
+a = 0
 
+dx = [0, 0, 1, -1]
+dy = [1, -1 ,0 , 0]
+def bfs(xa, ya):
+    queue = deque()
+    queue.append((xa, ya))
+    while queue:
+        x, y = queue.popleft()
+        field[x][y] = 0
+        for i in range(4):
+            nx = dx[i] + x
+            ny = dy[i] + y
 
-def dfs(x, y):
-    dx = [1, -1, 0, 0]
-    dy = [0, 0, 1, -1]
+            if 0 <= nx < n and 0<= ny < m and field[nx][ny] != 0:
+                field[nx][ny] = 0
+                queue.append((nx, ny))
+    return 1
 
-    # 상,하,좌,우 확인
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-
-        if (0 <= nx < N) and (0 <= ny < M):
-            if matrix[nx][ny] == 1:
-                matrix[nx][ny] = -1
-                dfs(nx, ny)
-
-
-T = int(input())
-for _ in range(T):
-    M, N, K = map(int, input().split())
-    matrix = [[0] * M for _ in range(N)]
+while a < t:
     cnt = 0
+    m, n, k = map(int, input().split())
 
-    for _ in range(K):
-        m, n = map(int, input().split())
-        matrix[n][m] = 1
+    field = [[0 for _ in range(m)] for _ in range(n)]
 
-    for i in range(N):
-        for j in range(M):
-            if matrix[i][j] > 0:
-                dfs(i, j)
-                cnt += 1
+    for _ in range(k):
+        x, y = map(int, input().split())
+        field[y][x] = 1
+    for i in range(n):
+        for j in range(m):
+            if field[i][j] == 1:
+                cnt+=bfs(i, j)
     print(cnt)
+    a+=1
